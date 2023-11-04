8d986e75ea9e3c74ab7475a83cc8272d79b9c350
SeneSatka <senesatka@gmail.com>
2023-11-04 21:24:07 +0300

    Edit: Test files and folders removed

--- test.js
+++
@@ -1,8 +0,0 @@
-const { consLogger } = require("./src/cjs/index.js");
-const logger = new consLogger({
-  time: true,
-  date: true,
-  path: "/logger",
-  dailyLog: true,
-});
-logger.console.log("Sa");
